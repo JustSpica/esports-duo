aa50dc15f45e93643c4d426d538713d925d7ff7f
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2022-09-20 18:56:40 -0300

    forgot packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,20 @@ module.exports = {
       sans: ['Inter', 'sans-serif']
     },
     extend: {
+      keyframes: {
+        show: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        dialog: {
+          '0%': { opacity: 0, transform: 'translate(-50%, -48%) scale(.96)' },
+          '100%': { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+        }
+      },
+      animation: {
+        'show': 'show 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        'dialog': 'dialog 150ms cubic-bezier(0.16, 1, 0.3, 1)'
+      },
       backgroundImage: {
         background: "url('/background.png')",
         arcoGradient: "linear-gradient(90deg, #9572FC 10%, #43E7AD 40%, #E1D55D 100%)",
@@ -13,5 +27,10 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-radix')({
+      variantPrefix: false,
+    }),
+    require('tailwind-scrollbar'),
+  ]
 }
